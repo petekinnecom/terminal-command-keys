f171b57c2fafc6db7604131dc165423e010839ab
Pete Kinnecom <git@k7u7.com>
2018-05-28 19:56:53 -0700

    Implement extension

--- README.md
+++ README.md
@@ -1,65 +1,82 @@
-# terminal-command-keys README
+# Terminal Command Keys
 
-This is the README for your extension "terminal-command-keys". After writing up a brief description, we recommend including the following sections.
+Assign terminal commands to a keybinding.
 
-## Features
+Open your keybindings.json (Command Palette => 'Preferences: Open Keyboard Shortcut File') and add an entries like these:
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+```
+  {
+      "key": "cmd+ctrl+r",
+      "command": "terminalCommandKeys.run",
+      "args": {
+          "cmd": "echo 'hello world'"
+      }
+  },
+  {
+      "key": "cmd+shift+r",
+      "command": "terminalCommandKeys.run",
+      "args": {
+          "cmd": "echo 'hello from file: ${file}'",
+          "newTerminal": false,
+          "saveAllFiles": true,
+          "showTerminal": true
+      }
+  }
+```
 
-For example if there is an image subfolder under your extension project workspace:
+The `args` entry has these entries:
 
-\!\[feature X\]\(images/feature-x.png\)
+---
+### "cmd" : required
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+This is the command that will be passed to the terminal when it is opened. The following substitutions are available to be used:
 
-## Requirements
+- ${line}
+- ${relativeFile}
+- ${file}
+- ${workspaceRoot}
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+Here are some examples:
 
-## Extension Settings
+- "cmd": "rspec ${file}"
+- "cmd": "rspec ${file}:${line}"
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
+If you need to run different commands for different file types, but wish to use the same keycombo, check out [vscode's "when" clauses](https://code.visualstudio.com/docs/getstarted/keybindings#_when-clause-contexts).
 
-For example:
+---
 
-This extension contributes the following settings:
+### "newTerminal" : optional, default false
 
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
+ If true, the terminal is destroyed and a fresh terminal is created for each run. You cannot scroll up to review previous commands/results.
 
-## Known Issues
+If false, re-uses the same terminal for each command run. You can scroll up to review previous commands/results.
 
-Calling out known issues can help limit users opening duplicate issues against your extension.
+---
 
-## Release Notes
+### "saveAllFiles" : optional, default true
 
-Users appreciate release notes as you update your extension.
+If true, saves all files before running the command.
 
-### 1.0.0
+If false, does not save all files before running the command.
 
-Initial release of ...
+---
 
-### 1.0.1
+### "showTerminal" : options, default true
 
-Fixed issue #.
+If true, ensures that the terminal is showing when running the command.
 
-### 1.1.0
+If false, does not change the visibility of the terminal when running the command.
 
-Added features X, Y, and Z.
+---
 
------------------------------------------------------------------------------------------------------------
+## About
 
-## Working with Markdown
+This extension inspired by [run-in-terminal](https://github.com/kortina/run-in-terminal) and [send-to-terminal](https://github.com/malkomalko/send-to-terminal).
 
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
+The differences (at this time):
 
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+- All config lives in keybindings file (no entries in user settings)
+- Configuration per command
+- Option to save all files before execution
+- Option to reveal the terminal before execution
+- Option to use a new terminal for each execution
--- extension.js
+++ extension.js
@@ -1,30 +1,125 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const path = require('path')
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// BEGIN TERMINAL
+//
+// _activeTerminal is a reference our terminal. However, sometimes we
+// dispose the terminal and sometimes the user does. The 'onDidCloseTerminal'
+// callback is called in both cases. This opens us to a timing issue, because we
+// could do:
+//
+//   _activeTerminal.dispose();  // _activeTerminal.id = 1
+//   _activeTerminal = createTerminal();  // _activeTerminal.id = 2
+//   onDidCloseTerminal runs and nulls out _activeTerminal with id 2  which is
+//     the new terminal we just created.
+//
+// To avoid this issue, we track 'tckDispose' so that we know whether the
+// event was triggered by the user or the extension.
+let _activeTerminal = null;
+vscode.window.onDidCloseTerminal((terminal) => {
+    if (terminal.name === 'terminal-command-keys') {
+        if (!terminal.tckDisposed) {
+            disposeTerminal();
+        }
+    }
+});
+function createTerminal() {
+    _activeTerminal = vscode.window.createTerminal('terminal-command-keys');
+    return _activeTerminal;
+}
+function disposeTerminal() {
+    _activeTerminal.tckDisposed = true;
+    _activeTerminal.dispose();
+    _activeTerminal = null;
+}
+function getTerminal(newTerminal) {
+    if (newTerminal && _activeTerminal) {
+        disposeTerminal();
+    }
+
+    if (!_activeTerminal) {
+        createTerminal();
+    }
+
+    return _activeTerminal;
+}
+// END TERMINAL
+
+function resolve(editor, command) {
+    var relativeFile = "." + editor.document.fileName.replace(vscode.workspace.rootPath, "");
+    var line = editor.selection.active.line + 1;
+
+    return command
+        .replace(/\${line}/g, `${line}`)
+        .replace(/\${relativeFile}/g, relativeFile)
+        .replace(/\${file}/g, `${editor.document.fileName}`)
+        .replace(/\${workspaceRoot}/g, `${vscode.workspace.rootPath}`);
+}
+
+function run(command, showTerminal, newTerminal) {
+    const terminal = getTerminal(newTerminal);
+
+    if (showTerminal) {
+        terminal.show(true);
+    }
+    terminal.sendText(command, true)
+}
+
+function warn(msg) {
+    console.log('terminalCommandKeys.run: ', msg)
+}
+
+function maybeSave(shouldSave) {
+    if (shouldSave) {
+        return vscode.workspace.saveAll(false)
+    } else {
+        return Promise.resolve()
+    }
+}
+
+function handleInput(editor, args) {
+    maybeSave(args.saveAllFiles).then(() => {
+        const cmd = resolve(
+            editor,
+            args.cmd
+        )
+
+        run(
+            cmd,
+            args.showTerminal,
+            args.newTerminal
+        );
+    });
+}
+
 function activate(context) {
+    let disposable = vscode.commands.registerCommand('terminalCommandKeys.run', (args) => {
+        const editor = vscode.window.activeTextEditor
+        if (!editor) {
+            warn('no active editor');
+            return;
+        }
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "terminal-command-keys" is now active!');
+        if (!args || !args.cmd) {
+            warn('keybinding must include a "args.cmd" key');
+            return;
+        }
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
+        const defaults = {
+            showTerminal: true,
+            saveAllFiles: true,
+            newTerminal: false
+        }
+        const realArgs = Object.assign(defaults, args)
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        handleInput(editor, realArgs)
     });
 
     context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {
+    disposeTerminal();
 }
 exports.deactivate = deactivate;
